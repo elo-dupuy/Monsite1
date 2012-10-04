5a282089a01014de5713ff14905bf74711458add
Elodie Dupuy <elodie.dupuy@iscom.org>
2012-10-04 13:08:35 +0200

    ajouts scripts

---
+++ Public/Scripts/app.js
@@ -0,0 +1,14 @@
+function check_name(name){
+if( name == "gabriel"){
+return "Win";
+}else if( name == "clement"){
+return "we find clement";
+}else{
+var res = "Truth" +name+ "is different from gabriel and clement";
+return res;
+}
+}
+
+$ (document).ready(function(){
+	alert(check_name ("gabriel"));
+});
